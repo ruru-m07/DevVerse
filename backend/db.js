4f9cbdec052ae5add1564318d0884de81fc469d1
ruru-m07 <142723369+ruru-m07@users.noreply.github.com>
2023-08-30 15:34:31 +0530

    done basic backend

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const mongoURI = process.env.MONGO_URI;
+
+const connectToMongo = async () => {
+  await mongoose.connect(mongoURI, () => {
+    console.log("Connected to Mongo Successfully");
+  });
+};
+
+
+module.exports = connectToMongo;
